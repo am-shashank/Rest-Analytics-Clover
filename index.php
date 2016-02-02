6e5807e0b00638f860634c95978f80f58db81b38
Prashanth C A <Prashanth C A>
2016-02-02 20:06:12 +0530

    ASDAD

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
 	<title>Clover - Rest Analytics</title>
 	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://bootswatch.com/cosmo/bootstrap.min.css"/>
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://bootswatch.com/cosmo/bootstrap.min.css"/>	
 	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<script type="text/javascript" src="/main.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<nav class="navbar-default">
