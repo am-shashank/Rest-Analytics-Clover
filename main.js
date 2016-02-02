6e44240b6c10ad6257ed3e2f76054e2141b59b17
Shashank Reddy <shashank.am@outlook.com>
2016-02-02 15:02:30 -0500

    fix itemCount map

--- main.js
+++ main.js
@@ -63,11 +63,11 @@ function fetchAllOrders(){
 				// fetch line items for every order
 				fetchLineItems(orderid);
 			}
-			console.log("itemCount");
-			console.log(itemCount);
+			//console.log("itemCount");
+			//console.log(itemCount);
 		},
 		complete: function(data) {
-			console.log(itemCount);
+			// console.log(itemCount);
 			// var maxCount = 0;
 			// var maxItem = "";
 			// for(var k in itemCount){
@@ -77,6 +77,8 @@ function fetchAllOrders(){
 			// 		maxItem = k.name;
 			// 	}
 			// }
+			console.log("MaxItem: " + maxItem);
+			console.log("MaxItemCount: " + maxCount);
 			$('#orders1 .panel-body').text(maxItem + ":" + maxCount);
 			$("#mask").fadeOut('slow',function(){
 				$("#mask").remove();
